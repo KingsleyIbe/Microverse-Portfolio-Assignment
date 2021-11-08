b82adb5cd58095a08b21b70fbe452ba79f68837f
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2021-11-08 18:36:30 +0100

    Fix linters errors

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 window.onload = () => {
   const hamburger = document.querySelector('.menu1');
   hamburger.addEventListener('click', () => {
-      const menu = document.querySelector('.mobile-menu');
-      menu.style.display = 'block';
+    const menu = document.querySelector('.mobile-menu');
+    menu.style.display = 'block';
   });
 
   const closeBtn = document.querySelector('.closeBtn');
   closeBtn.addEventListener('click', () => {
-      const menu = document.querySelector('.mobile-menu');
-      menu.style.display="none";
+    const menu = document.querySelector('.mobile-menu');
+    menu.style.display = 'none';
   });
 
   const backToMainPage = document.querySelector('.menuPopUp');
   backToMainPage.addEventListener('click', () => {
-      const menu = document.querySelector('.mobile-menu');
-      menu.style.display = 'none';
+    const menu = document.querySelector('.mobile-menu');
+    menu.style.display = 'none';
   });
 };
